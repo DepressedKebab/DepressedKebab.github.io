550f7267aebe7e8597672686f7cc53d09fede787
George Floridis <70459414+DepressedKebab@users.noreply.github.com>
2021-03-02 20:58:17 +0200

    ep.8 + optimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -287,47 +287,17 @@
 
 				var $this = $(this);
 
-				// Close.
-					$('<div class="close">Close</div>')
-						.appendTo($this)
-						.on('click', function() {
-							location.hash = '';
-						});
+
 
 				// Prevent clicks from inside article from bubbling.
-					$this.on('click', function(event) {
-						event.stopPropagation();
-					});
+				$this.on('click', function(event) {
+					event.stopPropagation();
+				});
 
 			});
 
 		// Events.
-			$body.on('click', function(event) {
-
-				// Article visible? Hide.
-					if ($body.hasClass('is-article-visible'))
-						$main._hide(true);
-
-			});
-
-			$window.on('keyup', function(event) {
-
-				switch (event.keyCode) {
-
-					case 27:
-
-						// Article visible? Hide.
-							if ($body.hasClass('is-article-visible'))
-								$main._hide(true);
-
-						break;
-
-					default:
-						break;
-
-				}
-
-			});
+			
 
 			$window.on('hashchange', function(event) {
 
